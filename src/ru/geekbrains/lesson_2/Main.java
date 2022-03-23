b99cb918fcb307b43ac98696c895268a8176bf18
katring28 <mactoob@mail.ru>
2022-03-23 12:46:06 +0300

    commit

---
+++ src/ru/geekbrains/lesson_2/Main.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.lesson_2;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Введите число");
+        Scanner skanner = new Scanner(System.in);
+        int a = skanner.nextInt();
+        System.out.println("a = "+ a);
+       // test
+    // write your code here
+    }
+}
